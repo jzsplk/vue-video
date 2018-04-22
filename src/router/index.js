8c7a13d6fe14e7cc4f68fb8d7cf29cbf74e7b760
Alex <jzsplking@gmail.com>
2018-04-22 21:38:56 +0800

    Feature：尝试增加video

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Video01 from '@/components/01-video'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/v1',
+      name: 'V1',
+      component: Video01
     }
   ]
 })
